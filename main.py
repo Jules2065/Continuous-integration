d9cbcf8450afbeed38c862fae5e511e65d5b06b2
Jules Solere <julessolere2065@gmail.com>
2023-04-02 19:19:57 +0200

    A finir

--- main.py
+++ main.py
@@ -8,59 +8,164 @@ from time import sleep
 
 app = Flask(__name__)
 
+USERS = ["Tata", "Toto", "Jules", "Idir", "Roger", "Natalie", "Bastien", "Sebastien", "Emilie", "Emile"]
+
+
+nbr_group = 5
+nbr_user = 21
+last_min = False
+group_list = []
+user_list = []
+true_nbr_group = 5
+user_group = 4
+user_group_spe = 5
+
+
+class Group():
+    def __init__(self, name) -> None:
+        self.name = name
+
+
+class User():
+    def __init__(self, name, group) -> None:
+        self.name = name
+        self.group = group
+
+
+
+for user in USERS:
+    user_list.append(User(user, "Sans groupe"))
+
 
 
 @app.route("/login-page/", methods=['GET', 'POST'])
 def login_page():
-    print("allo?")
+    global user_list
     if request.method == 'POST':
-        print("POST")
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-           pass
+        if request.form['username'] == 'admin':
+            #connexion comme utilisateur admin
+            return redirect(url_for('main_page_admin'))
+        else:
+            #connexion comme utilisateur normal
+            if request.form['username'] not in USERS:
+                USERS.append(request.form['username'])
+                user_list.append(User(request.form['username'], None))
+            return redirect(url_for('main_page_user'))
     return render_template('login-page.html')
 
-@app.route("/create-account", methods=['GET', 'POST'])
-def create_account():
+@app.route("/main-page-admin/", methods=['GET', 'POST'])
+def main_page_admin():
+    global nbr_group
+    global nbr_user
+    global last_min
+    global true_nbr_group
+    global group_list
+
+    info = ""
     if request.method == 'POST':
-        sql_client = SqlLogger()
-        if sql_client.connexion():
-            try:
-                sql_client.ecriture(f"INSERT INTO users VALUES ('{request.form['mail']}', '{request.form['pwd']}', '0', '{request.form['pseudo']}') ")
-                return redirect(url_for('login_page'))
-            except Exception as e:
-                print("Erreur as e : ", e)
-            sql_client.deconnexion()
-    return render_template('create-account.html')
-
-@app.route("/test/")
-def test():
-    last_min = True
-    nbr_user = 21
-    nbr_group = 6
-    if last_min:
-        while ceil(nbr_user/nbr_group) != (nbr_user - (nbr_group-1) * ceil(nbr_user/nbr_group)) +1:
-            nbr_group += 1 
-        if nbr_user % nbr_group == 0:
-            print(f"total de {nbr_group} groupes")
-            print("nombre de personnes dans tous les groupes :", 1)
+        if request.form['group'] == "":
+            nbr_group = 0
         else:
-            max_nbr_user_group = ceil(nbr_user/nbr_group)
-            print(f"total de {nbr_group} groupes")
-            print("nombre de personnes dans un groupe simple :", max_nbr_user_group)
-            print("nombre de personnes dans le dernier groupe :", nbr_user%max_nbr_user_group)
+            nbr_group = int(request.form['group'])
+
+        if request.form['user'] == "":
+            nbr_user = 0
+        else:
+            nbr_user = int(request.form['user'])
+        
+        if request.form['last'] == "":
+            last_min = True
+        elif request.form['last'] == "lastmin":
+            last_min = True
+        else:
+            last_min = False
+    group_list.clear()
+    info = calcul_group_user()
+
+    for i in range(true_nbr_group):
+        group_list.append(Group(f"Groupe {i+1}"))
+    group_list.append(Group("Sans groupe"))
+    return render_template('main-page-admin.html', info=info)
+
+@app.route("/main-page-user/", methods=['GET', 'POST'])
+def main_page_user():
+    global user_list
+    global group_list
+    info = calcul_group_user()
+
+    
+    #Création d'un tableau de dictionnaire pour afficher les groupes sur le site
+    print(len(group_list))
+    tab = []
+    for user in user_list:
+        dico = {}
+        for group in group_list:
+            dico[group.name] = ""
+            if user.group == group.name:
+                dico[group.name] = user.name
+        print("dico")
+        tab.append(dico)
+
+    print(tab)
+    
+    return render_template('main-page-user.html', info=info, tab=tab)
 
 
+
+
+def calcul_group_user():
+    global nbr_group
+    global nbr_user
+    global last_min
+    global true_nbr_group
+    global user_group
+    global user_group_spe
+    if nbr_user == 0 or nbr_group == 0:
+        info = f"Nombre de groupes maximum = 0 | Nombre de personnes par groupes = 0"
+        true_nbr_group = 0
+        user_group = 0
+        user_group_spe = 0
     else:
-        print((nbr_user - (nbr_group-1) * floor(nbr_user/nbr_group)))
-        print(floor(nbr_user/nbr_group))
-        while (nbr_user - (nbr_group-1) * floor(nbr_user/nbr_group)) != floor(nbr_user/nbr_group) +1 :
-            nbr_group +=1
         if nbr_user % nbr_group == 0:
-            print(f"total de {nbr_group} groupes")
-            print("nombre de personnes dans tous les groupes :", 1)
+            info = f"Nombre de groupes maximum = {nbr_group} | Nombre de personnes par groupes = {int(nbr_user/nbr_group)}"
+            true_nbr_group = nbr_group
+            user_group = int(nbr_user/nbr_group)
+            user_group_spe = 0
         else:
-            max_nbr_user_group = floor(nbr_user/nbr_group)
-            print(f"total de {nbr_group} groupes")
-            print("nombre de personnes dans un groupe simple:", max_nbr_user_group)
-            print("nombre de personnes dans le dernier groupe :", nbr_user - ((nbr_group-1) * (max_nbr_user_group)))
-    return render_template('test.html')  
+            if last_min:
+                while ceil(nbr_user/nbr_group) != (nbr_user - (nbr_group-1) * ceil(nbr_user/nbr_group)) +1:
+                    nbr_group += 1 
+                if nbr_user % nbr_group == 0:
+                    info = f"Nombre de groupes maximum = {nbr_group} | Nombre de personnes par groupes = 1"
+                    true_nbr_group = nbr_group
+                    user_group = 1
+                    user_group_spe = 0
+
+                else:
+                    max_nbr_user_group = ceil(nbr_user/nbr_group)
+                    info = f"Nombre de groupes maximum = {nbr_group} | Nombre de personnes par groupes simples = {max_nbr_user_group} | Nombre de personnes dans le dernier groupe = {nbr_user%max_nbr_user_group}"
+                    true_nbr_group = nbr_group
+                    user_group = max_nbr_user_group
+                    user_group_spe = nbr_user%max_nbr_user_group
+
+            else:
+                while (nbr_user - (nbr_group-1) * floor(nbr_user/nbr_group)) != floor(nbr_user/nbr_group) +1 :
+                    nbr_group +=1
+                if nbr_user % nbr_group == 0:
+                    info = f"Nombre de groupes maximum = {nbr_group} | Nombre de personnes par groupes = 1"
+                    true_nbr_group = nbr_group
+                    user_group = 1
+                    user_group_spe = 0
+                else:
+                    max_nbr_user_group = floor(nbr_user/nbr_group)
+                    info = f"Nombre de groupes maximum = {nbr_group} | Nombre de personnes par groupes simples = {max_nbr_user_group} | Nombre de personnes dans le dernier groupe = {nbr_user - ((nbr_group-1) * (max_nbr_user_group))}"
+                    true_nbr_group = nbr_group
+                    user_group = max_nbr_user_group
+                    user_group_spe = nbr_user - ((nbr_group-1) * (max_nbr_user_group))
+    return info
+info = calcul_group_user()
+print("info :", info)
+print('true_nbr_group :', true_nbr_group)
+for i in range(true_nbr_group):
+    group_list.append(Group(f"Groupe {i+1}"))
+group_list.append(Group("Sans groupe"))
